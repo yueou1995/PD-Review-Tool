a38581454b0ef909a04cd2d7eb1e23afae699889
Yue Ou <yuou@microsoft.com>
2018-06-14 15:40:38 -0700

    using Fabric UI

--- src/components/picture-dictionary-review-tool.tsx
+++ src/components/picture-dictionary-review-tool.tsx
@@ -2,13 +2,34 @@ import * as React from 'react';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { LanguageSelectionPage } from './language-selection-page';
+import { ReviewStage, ReviewState } from '../states/review.state';
 
 
+@observer
 export class PictureDictionaryReviewTool extends React.Component {
+
+  private _reviewState: ReviewState;
+
+  constructor() {
+    super();
+    this._reviewState = new ReviewState();
+  }
+
+
   render() {
+
+    let component: JSX.Element = null;
+
+    switch (this._reviewState.currentStage) {
+      case ReviewStage.LanguageSelection:
+        component = <LanguageSelectionPage />;
+        break;
+      //case ReviewStage.RoleSelection: component = 
+    }
+
     return (
       <div>
-        <LanguageSelectionPage />
+        {component}
       </div>
     );
   }
---
+++ src/states/review.state.ts
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------
+// <copyright file="review.state.ts" company="Microsoft">
+// © Microsoft. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+import { observable } from 'mobx';
+
+export enum ReviewStage {
+    LanguageSelection,
+    RoleSelection,
+    Review
+}
+
+export class ReviewState {
+  @observable
+  currentStage: ReviewStage = ReviewStage.LanguageSelection;  
+
+}
